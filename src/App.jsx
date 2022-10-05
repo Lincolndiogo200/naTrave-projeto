f908abfb4003b8fd232f17fc972a53d053ae5be3
Lincolndiogo200 <lincolndiogo200@gmail.com>
2022-10-04 21:19:29 -0300

    kkk

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './global.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      olá mundo!
+      </div>
+  )
+}
+
+export default App
